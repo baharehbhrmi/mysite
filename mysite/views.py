9a5aa00c3f292e812846cb750c0c156f61074c2c
baharehbhrmi18 <bahari18bahrami13f79@gmail.com>
2026-06-30 07:23:20 +0330

    Creating first view
    Created views.py in mysite and added the first view

---
+++ mysite/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse, JsonResponse
+
+
+def http_test(request):
+    return HttpResponse('<h1>This is a test</h1>')
+
+def json_test(request):
+    return JsonResponse({'name':'Ali'}) 
